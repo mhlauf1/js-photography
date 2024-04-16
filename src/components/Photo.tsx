db8008041c1822a30ce9b0a99d597234eca7711b
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-04-16 08:41:56 -0500

    trying to fix mobile flashing

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,95 +1,98 @@
 "use client";
-import { useMediaQuery } from "react-responsive";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Image from "next/image";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { IoMdArrowForward } from "react-icons/io";
+import { useMediaQuery } from "react-responsive";
+import { AnimationProps, MotionProps } from "framer-motion";
 
 type Props = {
   src: string;
   title: string;
   date: string;
   cols: string;
-  animation: any;
+  animation: AnimationProps; // assuming AnimationProps is a custom type you have defined that fits the structure you are passing
 };
 
-const Photo = ({ src, title, date, cols, animation }: Props) => {
-  const prefersReducedMotion = useMediaQuery({
-    query: "(prefers-reduced-motion: reduce)",
-  });
+const Photo: React.FC<Props> = React.memo(
+  ({ src, title, date, cols, animation }) => {
+    const prefersReducedMotion = useMediaQuery({
+      query: "(prefers-reduced-motion: reduce)",
+    });
 
-  const [hovered, setHovered] = useState(false);
-  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+    const [hovered, setHovered] = useState<boolean>(false);
+    const animationControls = useAnimation();
 
-  const [ref, inView] = useInView({
-    triggerOnce: true, // Trigger the animation only once
-    threshold: 0.15, // Adjust this value to control when the animation starts
-  });
-  const arrowVariants = {
-    hidden: { x: -20, opacity: 0 }, // Start closer to the final position
-    visible: {
-      x: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        stiffness: 100,
-        damping: 20,
-        duration: 1,
-      }, // Slower and sharper movement
-    },
-  };
+    const [ref, inView] = useInView({
+      triggerOnce: true,
+      threshold: 0.15,
+    });
 
-  const animationControls = useAnimation();
+    const handleMouseEnter = useCallback(() => {
+      setHovered(true);
+      animationControls.start("visible");
+    }, [animationControls]);
 
-  const handleMouseEnter = () => {
-    setHovered(true);
-    animationControls.start("visible");
-  };
+    console.log(title, "render");
 
-  const handleMouseLeave = () => {
-    setHovered(false);
-    animationControls.start("hidden");
-  };
+    const handleMouseLeave = useCallback(() => {
+      setHovered(false);
+      animationControls.start("hidden");
+    }, [animationControls]);
 
-  const animationProps = prefersReducedMotion
-    ? {}
-    : {
-        ...animation,
-        animate: inView ? animation.animate : animation.initial, // Animate when in view
-      };
-  return (
-    <motion.div
-      {...animationProps}
-      ref={ref}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={`w-full md:w-${cols}/12 text-gray-400 hover:text-white duration-500 cursor-pointer`}
-    >
-      <Image
-        alt={title}
-        src={src}
-        width={500}
-        height={500}
-        className="rounded-lg opacity-80 hover:opacity-100 duration-500"
-        objectFit="cover"
-        layout="responsive"
-      />
-      <div className="flex mt-2 justify-between items-start">
-        <div className="bg-[#040404] flex flex-col flex-1">
-          <h3>{title}</h3>
-          <p className="text-sm">{date}</p>
+    const arrowVariants = {
+      hidden: { x: -20, opacity: 0 },
+      visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.5, // Reduced duration
+          ease: "easeOut", // Changed easing function
+        },
+      },
+    };
+
+    const animationProps = prefersReducedMotion
+      ? {}
+      : {
+          ...animation,
+          animate: inView ? animation.animate : animation.initial,
+        };
+
+    return (
+      <motion.div
+        {...animationProps}
+        ref={ref}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={`w-full md:w-${cols}/12 text-gray-400 hover:text-white duration-500 cursor-pointer`}
+      >
+        <Image
+          alt={title}
+          src={src}
+          width={500}
+          height={500}
+          className="rounded-lg opacity-80 hover:opacity-100 duration-500"
+          objectFit="cover"
+          layout="responsive"
+        />
+        <div className="flex mt-2 justify-between items-start">
+          <div className="bg-[#040404] flex flex-col flex-1">
+            <h3>{title}</h3>
+            <p className="text-sm">{date}</p>
+          </div>
+          <motion.div
+            variants={arrowVariants}
+            initial="hidden"
+            animate={hovered ? "visible" : "hidden"}
+          >
+            <IoMdArrowForward className="text-xl" />
+          </motion.div>
         </div>
-        <motion.div
-          variants={arrowVariants}
-          initial="hidden"
-          animate={hovered ? "visible" : "hidden"}
-        >
-          <IoMdArrowForward className="text-xl" />
-        </motion.div>
-      </div>
-    </motion.div>
-  );
-};
+      </motion.div>
+    );
+  }
+);
 
 export default Photo;
